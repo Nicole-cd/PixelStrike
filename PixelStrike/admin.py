cf7f150c487d8a0014e2c5ea827bdd29e010caed
sha1d <shadst7@gmail.com>
2026-07-31 14:41:24 -0400

    agregar modelos de contenido dinámico

--- PixelStrike/admin.py
+++ PixelStrike/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import favoritos
-# Register your models here.
+
+from .models import Genero, Juego, Noticia, favoritos
+
+
 class favoritosAdmin(admin.ModelAdmin):
     list_display = ('nombreFavorito', 'descripcionFavorito')
+    search_fields = ('nombreFavorito', 'descripcionFavorito')
+
 
 admin.site.register(favoritos, favoritosAdmin)
+
+
+@admin.register(Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'orden', 'activo')
+    list_editable = ('orden', 'activo')
+    list_filter = ('activo',)
+    search_fields = ('nombre',)
+
+
+@admin.register(Juego)
+class JuegoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'categoria', 'destacado')
+    list_filter = ('categoria', 'destacado')
+    search_fields = ('titulo', 'descripcion', 'categoria')
+
+
+@admin.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_publicacion')
+    search_fields = ('titulo', 'contenido')
+    readonly_fields = ('fecha_publicacion',)
